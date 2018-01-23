49ac58f5837d6b151bdecd689d601130ba3d57ed
Alexander Baratov <baratov@outlook.com>
2018-01-23 21:51:48 +0300

    Hello world web app

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import(
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func handler(w http.ResponseWriter, req *http.Request) {
+	w.Write([]byte("OK"))
+}
