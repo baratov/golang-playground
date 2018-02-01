82b46ec6c233e59b88d85e1dc7c86e0648df8dba
Alexander Baratov <baratov@outlook.com>
2018-02-01 22:05:03 +0300

    store_test code cleanup

--- store/store_test.go
+++ store/store_test.go
@@ -12,11 +12,9 @@ func TestGet(t *testing.T) {
 	store.Set("someKey", 123)
 
 	val, err := store.Get("someKey")
-
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
-
 	if val != 123 {
 		t.Errorf("Expected value is 123, but found %s", val)
 	}
@@ -26,12 +24,10 @@ func TestGet_NonExistingKey(t *testing.T) {
 	store := store.New()
 
 	val, err := store.Get("someKey")
-
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected { // hmm
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
 	}
-
 	if val != nil {
 		t.Errorf("Expected value is nil, but found %s", val)
 	}
@@ -42,17 +38,14 @@ func TestUpdate(t *testing.T) {
 	store.Set("someKey", 123)
 
 	err := store.Update("someKey", 234)
-
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
 
 	val, err := store.Get("someKey")
-
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
-
 	if val != 234 {
 		t.Errorf("Expected value is 234, but found %s", val)
 	}
@@ -60,8 +53,8 @@ func TestUpdate(t *testing.T) {
 
 func TestUpdate_NonExistingKey(t *testing.T) {
 	store := store.New()
-	err := store.Update("someKey", 234)
 
+	err := store.Update("someKey", 234)
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -78,12 +71,10 @@ func TestDelete(t *testing.T) {
 	}
 
 	val, err := store.Get("someKey")
-
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
 	}
-
 	if val != nil {
 		t.Errorf("Expected value is nil, but found %s", val)
 	}
@@ -91,8 +82,8 @@ func TestDelete(t *testing.T) {
 
 func TestDelete_NonExistingKey(t *testing.T) {
 	store := store.New()
-	err := store.Delete("someKey")
 
+	err := store.Delete("someKey")
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
