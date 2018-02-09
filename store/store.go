39713ffa07d1284481855d0dab1514bf50cf297a
Alexander Baratov <baratov@outlook.com>
2018-02-09 23:44:15 +0300

    Basic ttl added

--- store/store.go
+++ store/store.go
@@ -3,18 +3,28 @@ package store
 import (
 	"fmt"
 	"sync"
+	"time"
 )
 
 const keyNotFoundFmt = "key '%s' not found"
 
+type item struct {
+	value      interface{} // interface{} says nothing
+	expiration time.Time
+}
+
+func (item *item) isExpired() bool {
+	return time.Now().After(item.expiration)
+}
+
 type Store struct {
-	mutex sync.RWMutex           // https://github.com/golang/go/wiki/MutexOrChannel
-	items map[string]interface{} // sync.Map could give synchronization out of the box
-} // interface{} says nothing
+	mutex sync.RWMutex    // https://github.com/golang/go/wiki/MutexOrChannel
+	items map[string]item // sync.Map could give synchronization out of the box
+}
 
 func New() *Store {
 	return &Store{
-		items: make(map[string]interface{}),
+		items: make(map[string]item),
 	}
 }
 
@@ -26,35 +36,45 @@ func (store *Store) Get(key string) (interface{}, error) {
 }
 
 func (store *Store) get(key string) (interface{}, error) {
-	value, ok := store.items[key]
-	if ok {
-		return value, nil
+	i, ok := store.items[key]
+	if ok && !i.isExpired() {
+		return i.value, nil
 	}
 	return nil, fmt.Errorf(keyNotFoundFmt, key)
 }
 
-func (store *Store) Set(key string, value interface{}) {
+func (store *Store) Set(key string, value interface{}, ttl time.Duration) {
+	i := item{
+		value:      value,
+		expiration: time.Now().Add(ttl),
+	}
+
 	store.mutex.Lock()
 	defer store.mutex.Unlock()
 
-	store.set(key, value)
+	store.set(key, i)
 }
 
-func (store *Store) set(key string, value interface{}) {
-	store.items[key] = value
+func (store *Store) set(key string, i item) {
+	store.items[key] = i
 }
 
-func (store *Store) Update(key string, value interface{}) error {
+func (store *Store) Update(key string, value interface{}, ttl time.Duration) error {
+	i := item{
+		value:      value,
+		expiration: time.Now().Add(ttl),
+	}
+
 	store.mutex.Lock()
 	defer store.mutex.Unlock()
 
-	return store.update(key, value)
+	return store.update(key, i)
 }
 
-func (store *Store) update(key string, value interface{}) error {
+func (store *Store) update(key string, i item) error {
 	_, err := store.get(key)
 	if err == nil {
-		store.items[key] = value
+		store.items[key] = i
 	}
 	return err
 }
@@ -79,7 +99,6 @@ func (store *Store) Keys() []string {
 
 func (store *Store) keys() []string {
 	keys := make([]string, len(store.items))
-
 	i := 0
 	for key := range store.items {
 		keys[i] = key
--- store/store_test.go
+++ store/store_test.go
@@ -5,11 +5,12 @@ import (
 	"runtime"
 	"strings"
 	"testing"
+	"time"
 )
 
 func TestGet(t *testing.T) {
 	store := store.New()
-	store.Set("someKey", 123)
+	store.Set("someKey", 123, time.Second)
 
 	val, err := store.Get("someKey")
 	if err != nil {
@@ -33,11 +34,26 @@ func TestGet_NonExistingKey(t *testing.T) {
 	}
 }
 
+func TestGet_ExpiredKey(t *testing.T) {
+	store := store.New()
+	store.Set("someKey", 123, time.Second)
+	time.Sleep(time.Second)
+
+	val, err := store.Get("someKey")
+	expected := "key 'someKey' not found"
+	if actual := err.Error(); actual != expected {
+		t.Errorf("Expected error is %s, but found %s", expected, actual)
+	}
+	if val != nil {
+		t.Errorf("Expected value is nil, but found %s", val)
+	}
+}
+
 func TestUpdate(t *testing.T) {
 	store := store.New()
-	store.Set("someKey", 123)
+	store.Set("someKey", 123, time.Second)
 
-	err := store.Update("someKey", 234)
+	err := store.Update("someKey", 234, time.Second)
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
@@ -54,7 +70,7 @@ func TestUpdate(t *testing.T) {
 func TestUpdate_NonExistingKey(t *testing.T) {
 	store := store.New()
 
-	err := store.Update("someKey", 234)
+	err := store.Update("someKey", 234, time.Second)
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -63,7 +79,7 @@ func TestUpdate_NonExistingKey(t *testing.T) {
 
 func TestDelete(t *testing.T) {
 	store := store.New()
-	store.Set("someKey", 123)
+	store.Set("someKey", 123, time.Second)
 
 	store.Delete("someKey")
 
@@ -79,9 +95,9 @@ func TestDelete(t *testing.T) {
 
 func TestKeys(t *testing.T) {
 	store := store.New()
-	store.Set("someKey", 123)
-	store.Set("otherKey", 234)
-	store.Set("someKey", 345)
+	store.Set("someKey", 123, time.Second)
+	store.Set("otherKey", 234, time.Second)
+	store.Set("someKey", 345, time.Second)
 
 	keys := store.Keys()
 	if len(keys) != 2 || keys[0] != "someKey" || keys[1] != "otherKey" { // the order is not guaranteed I guess
@@ -102,15 +118,15 @@ func TestKey_EmptyStore(t *testing.T) {
 }
 
 func TestConcurrentAccess(t *testing.T) {
-	runtime.GOMAXPROCS(runtime.NumCPU())
+	runtime.GOMAXPROCS(runtime.NumCPU()) //test won't work on single-core CPU
 	t.Parallel()
 
 	store := store.New()
 
 	for i := 0; i < 100000; i++ {
-		go store.Set("key", i)
+		go store.Set("key", i, time.Second)
 		go store.Get("key")
-		go store.Update("key", i)
+		go store.Update("key", i, time.Second)
 		go store.Delete("key")
 		go store.Keys()
 	}
