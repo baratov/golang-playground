736d77befe8baf352d7ee6f7e3e0debfbb8be136
Alexander Baratov <baratov@outlook.com>
2018-02-14 22:48:59 +0300

    Goway renaming

--- store/store_test.go
+++ store/store_test.go
@@ -9,10 +9,10 @@ import (
 )
 
 func TestGet(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
 
-	val, err := store.Get("someKey")
+	val, err := s.Get("someKey")
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
@@ -22,9 +22,9 @@ func TestGet(t *testing.T) {
 }
 
 func TestGet_NonExistingKey(t *testing.T) {
-	store := store.New()
+	s := store.New()
 
-	val, err := store.Get("someKey")
+	val, err := s.Get("someKey")
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected { // hmm
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -35,11 +35,11 @@ func TestGet_NonExistingKey(t *testing.T) {
 }
 
 func TestGet_ExpiredKey(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
 	time.Sleep(time.Second)
 
-	val, err := store.Get("someKey")
+	val, err := s.Get("someKey")
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -50,15 +50,15 @@ func TestGet_ExpiredKey(t *testing.T) {
 }
 
 func TestUpdate(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
 
-	err := store.Update("someKey", 234, time.Second)
+	err := s.Update("someKey", 234, time.Second)
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
 
-	val, err := store.Get("someKey")
+	val, err := s.Get("someKey")
 	if err != nil {
 		t.Errorf("Error found %s", err.Error())
 	}
@@ -68,9 +68,9 @@ func TestUpdate(t *testing.T) {
 }
 
 func TestUpdate_NonExistingKey(t *testing.T) {
-	store := store.New()
+	s := store.New()
 
-	err := store.Update("someKey", 234, time.Second)
+	err := s.Update("someKey", 234, time.Second)
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -78,11 +78,11 @@ func TestUpdate_NonExistingKey(t *testing.T) {
 }
 
 func TestUpdate_ExpiredKey(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
 	time.Sleep(time.Second)
 
-	err := store.Update("someKey", 234, time.Second)
+	err := s.Update("someKey", 234, time.Second)
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -90,12 +90,12 @@ func TestUpdate_ExpiredKey(t *testing.T) {
 }
 
 func TestDelete(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
 
-	store.Delete("someKey")
+	s.Delete("someKey")
 
-	val, err := store.Get("someKey")
+	val, err := s.Get("someKey")
 	expected := "key 'someKey' not found"
 	if actual := err.Error(); actual != expected {
 		t.Errorf("Expected error is %s, but found %s", expected, actual)
@@ -106,12 +106,12 @@ func TestDelete(t *testing.T) {
 }
 
 func TestKeys(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
-	store.Set("otherKey", 234, time.Second)
-	store.Set("someKey", 345, time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
+	s.Set("otherKey", 234, time.Second)
+	s.Set("someKey", 345, time.Second)
 
-	keys := store.Keys()
+	keys := s.Keys()
 	if len(keys) != 2 || keys[0] != "someKey" || keys[1] != "otherKey" { // the order is not guaranteed I guess
 		expected := strings.Join([]string{"someKey", "otherKey"}, ",")
 		actual := strings.Join(keys, ",")
@@ -120,9 +120,9 @@ func TestKeys(t *testing.T) {
 }
 
 func TestKeys_EmptyStore(t *testing.T) {
-	store := store.New()
+	s := store.New()
 
-	keys := store.Keys()
+	keys := s.Keys()
 	if len(keys) != 0 {
 		actual := strings.Join(keys, ",")
 		t.Errorf("Expected array is empty, but found [%s]", actual)
@@ -130,12 +130,12 @@ func TestKeys_EmptyStore(t *testing.T) {
 }
 
 func TestKeys_ExpiredKey(t *testing.T) {
-	store := store.New()
-	store.Set("someKey", 123, time.Second)
-	store.Set("otherKey", 123, 2*time.Second)
+	s := store.New()
+	s.Set("someKey", 123, time.Second)
+	s.Set("otherKey", 123, 2*time.Second)
 	time.Sleep(time.Second)
 
-	keys := store.Keys()
+	keys := s.Keys()
 	expected := "otherKey"
 	if len(keys) != 1 || keys[0] != expected {
 		actual := strings.Join(keys, ",")
