7d3502330b823d4b80d7f9823421db2ef10d0b89
Alexander Baratov <baratov@outlook.com>
2018-02-26 09:00:51 +0300

    Client implementation + integration tests

--- client/client.go
+++ client/client.go
@@ -1,26 +1,31 @@
 package client
 
 import (
+	"bytes"
 	"encoding/base64"
+	"encoding/json"
+	"errors"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
 	"time"
 )
 
-type HttpClient interface {
+type httpClient interface {
 	Do(*http.Request) (*http.Response, error)
 }
 
-type ClientFunc func(*http.Request) (*http.Response, error)
+type clientFunc func(*http.Request) (*http.Response, error)
 
-func (c ClientFunc) Do(r *http.Request) (*http.Response, error) {
+func (c clientFunc) Do(r *http.Request) (*http.Response, error) {
 	return c(r)
 }
 
-type Middleware func(HttpClient) HttpClient
+type Middleware func(httpClient) httpClient
 
 func BasicAuthorization(username, password string) Middleware {
-	return func(c HttpClient) HttpClient {
+	return func(c httpClient) httpClient {
 		inner := func(r *http.Request) (*http.Response, error) {
 			header := []byte(username + ":" + password)
 			encoded := base64.StdEncoding.EncodeToString(header)
@@ -28,39 +33,41 @@ func BasicAuthorization(username, password string) Middleware {
 			r.Header.Add("Authorization", "Basic "+encoded)
 			return c.Do(r)
 		}
-		return ClientFunc(inner)
+		return clientFunc(inner)
 	}
 }
 
-func LogResponseTime() Middleware {
-	return func(c HttpClient) HttpClient {
+func LogLatency() Middleware {
+	return func(c httpClient) httpClient {
 		inner := func(r *http.Request) (*http.Response, error) {
 			start := time.Now()
 			defer func() {
 				end := time.Now()
-				log.Printf("Request to %s took %v", r.URL.String(), end.Sub(start).Nanoseconds())
+				log.Printf("%v request to %v took %v miliseconds",
+					r.Method, r.URL.String(), end.Sub(start).Seconds()*100)
 			}()
 			return c.Do(r)
 		}
-		return ClientFunc(inner)
+		return clientFunc(inner)
 	}
 }
 
 type Client struct {
-	httpClient HttpClient
+	httpClient httpClient
 	apiUrl     string
 }
 
 const (
 	apiVersion = "v1/"
-	apiPath = "api/" + apiVersion + "keys/"
-	healthCheckPath = "health/"
+	apiPath    = "api/" + apiVersion + "keys/"
 )
 
 func New(apiUrl string, mw ...Middleware) *Client {
-	var httpClient HttpClient
+	var httpClient httpClient = &http.Client{
+		Timeout: time.Second * 10,
+	}
 	for _, middleware := range mw {
-		httpClient = middleware(&http.Client{})
+		httpClient = middleware(httpClient)
 	}
 	return &Client{
 		httpClient: httpClient,
@@ -68,7 +75,85 @@ func New(apiUrl string, mw ...Middleware) *Client {
 	}
 }
 
-func (c *Client) Get(key string) error {
+func (c *Client) Get(key string) (interface{}, error) {
+	resp, err := c.makeRequest("GET", key, nil)
+	if err != nil {
+		return nil, err
+	}
+	return getValueFromResponse(resp)
+}
 
-	return nil
+func (c *Client) Set(key string, value interface{}, ttl time.Duration) error {
+	payload, err := c.getPayload(value, ttl)
+	if err != nil {
+		return err
+	}
+	resp, err := c.makeRequest("POST", key, payload)
+	if err != nil {
+		return err
+	}
+	_, err = getValueFromResponse(resp)
+	return err
+}
+
+func (c *Client) Update(key string, value interface{}, ttl time.Duration) error {
+	payload, err := c.getPayload(value, ttl)
+	if err != nil {
+		return err
+	}
+	resp, err := c.makeRequest("PUT", key, payload)
+	if err != nil {
+		return err
+	}
+	_, err = getValueFromResponse(resp)
+	return err
+}
+
+func (c *Client) Delete(key string) error {
+	resp, err := c.makeRequest("DELETE", key, nil)
+	if err != nil {
+		return err
+	}
+	_, err = getValueFromResponse(resp)
+	return err
+}
+
+type Payload struct {
+	Value interface{}   `json:"value"`
+	Ttl   time.Duration `json:"ttl"`
+}
+
+func (c *Client) getPayload(value interface{}, ttl time.Duration) (io.Reader, error) {
+	p := Payload{Value: value, Ttl: ttl}
+	jStr, err := json.Marshal(p)
+	if err != nil {
+		return nil, err
+	}
+	return bytes.NewBuffer(jStr), nil
+}
+
+func (c *Client) makeRequest(method string, key string, body io.Reader) (*http.Response, error) {
+	req, err := http.NewRequest(method, c.apiUrl+apiPath+key, body)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("Content-Type", "application/json")
+	return c.httpClient.Do(req)
+}
+
+func getValueFromResponse(r *http.Response) (interface{}, error) {
+	body, err := ioutil.ReadAll(r.Body)
+	defer r.Body.Close()
+	if err != nil {
+		return nil, err
+	}
+	var resp map[string]interface{}
+	err = json.Unmarshal(body, &resp)
+	if err != nil {
+		return nil, err
+	}
+	if msg := resp["message"]; msg != nil {
+		return nil, errors.New(msg.(string))
+	}
+	return resp["data"], nil
 }
--- client/client_test.go
+++ client/client_test.go
@@ -1,8 +1,78 @@
 package client_test
 
+// integration tests for main use-cases
+// server running on localhost:8080 is required
+
 import (
 	"github.com/baratov/golang-playground/client"
+	"testing"
+	"time"
 )
 
-// some examples of client usage
+func TestSetGet(t *testing.T) {
+	c := client.New("http://localhost:8080/",
+		client.BasicAuthorization("username", "password"),
+		client.LogLatency())
 
+	origVal := "some_string_value"
+	err := c.Set("testKey", origVal, time.Second)
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+
+	val, err := c.Get("testKey")
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+	if val != origVal {
+		t.Errorf("Excpected value is %v, but found %v", origVal, val)
+	}
+}
+
+func TestUpdate(t *testing.T) {
+	c := client.New("http://localhost:8080/",
+		client.BasicAuthorization("username", "password"),
+		client.LogLatency())
+
+	err := c.Set("testKey", "some_string_value", time.Second)
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+
+	err = c.Update("testKey", "some_updated_value", time.Second)
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+
+	val, err := c.Get("testKey")
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+	if val != "some_updated_value" {
+		t.Errorf("Excpected value is %v, but found %v", "some_updated_value", val)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	c := client.New("http://localhost:8080/",
+		client.BasicAuthorization("username", "password"),
+		client.LogLatency())
+
+	err := c.Set("testKey", "some_string_value", time.Second)
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+	err = c.Delete("testKey")
+	if err != nil {
+		t.Errorf("Error found: %v", err.Error())
+	}
+
+	val, err := c.Get("testKey")
+	expected := "key 'testKey' not found"
+	if actual := err.Error(); actual != expected {
+		t.Errorf("Expected error is %v, but found %v", expected, actual)
+	}
+	if val != nil {
+		t.Errorf("Expected value is nil, but found %v", val)
+	}
+}
